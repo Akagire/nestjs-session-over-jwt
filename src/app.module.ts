b1fa48ba1970b9b46aa936252e100be6385b378c
Takuya Eguchi <seal2501@gmail.com>
2023-01-01 15:06:09 +0900

    chore: connect mongo db for session store

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://db:27017/local')],
   controllers: [AppController],
   providers: [AppService],
 })
